666737146f24c2111668c70dbfdfcbf7392f950b
yuapX <shesterovmm@gmail.com>
2024-08-24 20:58:18 +0300

    fea(common dto): implement common pagination

--- src/domain/exceptions/base.py
+++ src/domain/exceptions/base.py
@@ -1,6 +1,12 @@
 from typing import Optional
+from typing_extensions import Self
 
 
 class DomainError(Exception):
+    def __init__(self, message: Optional[str] = None):
+        self.message = message
+
+
+class ValueObjectError(ValueError):
     def __init__(self, message: Optional[str] = None):
         self.message = message
